4ef1450b974b76112c0269338f343371f5d54620
Artenes Nogueira <artenesama@gmail.com>
2019-09-20 13:36:07 -0400

    feat: Plays animation and redirect afterwards

--- src/pages/Play/index.js
+++ src/pages/Play/index.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
-export default function Play({ location }) {
+export default function Play({ location, history }) {
   const params = new URLSearchParams(location.search);
-  
+  const [isAnimating, setIsAnimating] = useState(false);
+  const [isShowingHours, setIsShowingHours] = useState('');
+
+  setTimeout(() => { setIsAnimating(true); }, 2000);
+
+  useEffect(() => {
+    if (isAnimating) {
+      document.getElementById('sound').play();
+      setTimeout(() => { setIsShowingHours('appear'); }, 2000);
+      setTimeout(() => { 
+        let target = params.get('target');
+        if (target) {
+
+          if (!target.startsWith('https://') || !target.startsWith('http://')) {
+            target = `https://${target}`;
+          }
+
+          window.location = target;
+        }
+      }, 5000);
+    }
+  }, [history, isAnimating, params]);
+
   return (
     <div id="title-card">
 
-      <h2>Dawn of</h2>
-      <h1>{params.get('title')}</h1>
-      <h3>-{params.get('subtitle')}-</h3>
+      {isAnimating && (
+      <>
+        <audio id="sound">
+          <source src="dawn.mp3" type="audio/mpeg"/>
+          <source src="dawn.ogg" type="audio/ogg"/>
+        </audio>
+        <h2>Dawn of</h2>
+        <h1>{params.get('title')}</h1>
+        <h3 className={isShowingHours}>-{params.get('subtitle')}-</h3>
+      </>
+      )}
 
     </div>
   );

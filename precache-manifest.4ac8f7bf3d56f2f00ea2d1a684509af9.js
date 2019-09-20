5214033a34177846c2a25d7d7fb70ab6645fc933
Artenes Nogueira <artenesama@gmail.com>
2019-09-20 14:21:11 -0400

    Updates

---
+++ precache-manifest.4ac8f7bf3d56f2f00ea2d1a684509af9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b6dfb967dadcb6cb8261b55603802a",
+    "url": "/majoras-mask-daw-day-generator/index.html"
+  },
+  {
+    "revision": "c90ab11ac5f5df893471",
+    "url": "/majoras-mask-daw-day-generator/static/css/main.7fa268d5.chunk.css"
+  },
+  {
+    "revision": "c1eec667a210b4325e0b",
+    "url": "/majoras-mask-daw-day-generator/static/js/2.68783f57.chunk.js"
+  },
+  {
+    "revision": "c90ab11ac5f5df893471",
+    "url": "/majoras-mask-daw-day-generator/static/js/main.01d7cf13.chunk.js"
+  },
+  {
+    "revision": "9bf91204b80ece20dca4",
+    "url": "/majoras-mask-daw-day-generator/static/js/runtime-main.b18fa126.js"
+  }
+]);
